4bff76d4843e9f17145c302671037a43fc15b7c7
Cyril Schumacher <cyril_schumacher@live.fr>
2025-09-25 17:43:01 +0200

    test: configure Playwright for UI testing

---
+++ playwright.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: 'test/ui',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:3000',
+    trace: 'on-first-retry',
+  }
+});
