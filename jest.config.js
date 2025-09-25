62c567ac6350e922f793a4c3a88592f0ca2e7126
Cyril Schumacher <cyril_schumacher@live.fr>
2025-09-25 18:11:24 +0200

    ci: add job for unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['test/unit'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '^@flexpect/(.*)$': '<rootDir>/src/$1',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
