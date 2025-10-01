f040d93d5e5a1d085fa773b636529750017f7e04
Cyril Schumacher <cyril_schumacher@live.fr>
2025-10-01 16:33:22 +0200

    chore: configure release rules to ignore chore commits

--- release.config.js
+++ release.config.js
@@ -21,6 +21,10 @@ module.exports = {
             type: 'feat',
             release: 'minor',
           },
+          {
+            type: 'chore',
+            release: false,
+          },
         ],
       },
     ],
